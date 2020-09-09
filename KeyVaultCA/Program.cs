70d663dff596a37900d4d168b9edaabde28c3332
Vitaliy Slepakov <v.slepakov@gmail.com>
2020-09-09 16:03:07 +0200

    added cmd options

--- KeyVaultCA/Program.cs
+++ KeyVaultCA/Program.cs
@@ -1,23 +1,46 @@
-﻿using System;
+﻿using CommandLine;
 using System.IO;
 
 namespace KeyVaultCA
 {
     class Program
     {
+        public class Options
+        {
+            [Option("appId", Required = true, HelpText = "AppId of the AAD service principal that can access KeyVault.")]
+            public string AppId { get; set; }
+
+            [Option("secret", Required = true, HelpText = "Password of the AAD service principal that can access KeyVault.")]
+            public string Secret { get; set; }
+
+            [Option("issuer", Required = true, HelpText = "Name of the issuing certificate in KeyVault.")]
+            public string IssuerCertName { get; set; }
+
+            [Option("csrPath", Required = true, HelpText = "Path to the CSR file in .der format")]
+            public string PathToCsr { get; set; }
+
+            [Option("output", Required = true, HelpText = "Output file name for the certificate")]
+            public string OutputFileName { get; set; }
+
+            [Option("kvName", Required = true, HelpText = "KeyVault name")]
+            public string KeyVaultName { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            var keyVaultServiceClient = new KeyVaultServiceClient("https://my-kv.vault.azure.net/");
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       var keyVaultServiceClient = new KeyVaultServiceClient($"https://{o.KeyVaultName}.vault.azure.net/");
 
-            var appId = Environment.GetEnvironmentVariable("APP_ID");
-            var appSecret = Environment.GetEnvironmentVariable("APP_SECRET");
-            keyVaultServiceClient.SetAuthenticationClientCredential(appId, appSecret);
+                       keyVaultServiceClient.SetAuthenticationClientCredential(o.AppId, o.Secret);
 
-            var kvCertProvider = new KeyVaultCertificateProvider("DeviceRootCA", keyVaultServiceClient);
-            var csr = File.ReadAllBytes("my-test-device-csr.der");
-            var cert = kvCertProvider.SigningRequestAsync(csr).Result;
+                       var kvCertProvider = new KeyVaultCertificateProvider(o.IssuerCertName, keyVaultServiceClient);
+                       var csr = File.ReadAllBytes(o.PathToCsr);
+                       var cert = kvCertProvider.SigningRequestAsync(csr).Result;
 
-            File.WriteAllBytes("cert.cer", cert.Export(System.Security.Cryptography.X509Certificates.X509ContentType.Cert));
+                       File.WriteAllBytes(o.OutputFileName, cert.Export(System.Security.Cryptography.X509Certificates.X509ContentType.Cert));
+                   });
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# keyvault-ca
+# Some Context
+Please refer to [this blog post](ADD LINK) for details.
+
+# Getting Started
+
+## Prerequisites
+
+1. Create Azure KeyVault:  
+```az keyvault create --name my-keyvault-ca \```  
+```--resource-group keyvault-ca \```  
+```--enable-soft-delete=true \```  
+```--enable-purge-protection=true```  
+
+2. Let KeyVault generate a Root certificate using the specified policy:  
+```az keyvault certificate create --vault-name my-keyvault-ca -n ContosoRootCA -p @kv-certPolicy.json```
+
+## Setup KeyVault access for the API façade
+
+1. Create a Service Principal in AAD  
+```az ad sp create-for-rbac --name api-facade-for-keyvault-ca --skip-assignment=true```  
+You will get an output containing ```appId``` and ```password```, please note them for later.
+
+2. Give the Service Principal accesss to KeyVault keys and certificates:  
+```az keyvault set-policy --name my-keyvault-ca \```  
+```--spn <your appId> \```  
+```--key-permissions get list sign \```  
+``` --certificate-permissions get list```  
+
+## Request a new device certificate
+
+1. First generate the private key:  
+```openssl genrsa -out mydevice.key 2048```  
+
+2. Create the CSR ():  
+```openssl req -new -key mydevice.key -out mydevice.csr```  
+```openssl req -in mydevice.csr -out mydevice.csr.der -outform DER```
+
+3. Run the API Facade and pass all required arguments:  
+```cd KeyVaultCA```  
+```dotnet run --appId <YOUR_APPID> --secret <YOUR_APP_SECRET> \```  
+```--issuer ContosoRootCA --csrPath <PATH_TO_CSR_IN_DER_FORMAT> \```  
+```--output <OUTPUT_CERTIFICATE_FILENAME> --kvName my-keyvault-ca```
