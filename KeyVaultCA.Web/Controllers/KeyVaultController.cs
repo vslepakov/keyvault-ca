6c16127b83f7e6269b8fef1df16ef0a1b59129e7
Vitaliy Slepakov <v.slepakov@gmail.com>
2020-12-07 14:52:16 +0100

    added web api adapter for keyvault

---
+++ KeyVaultCA.Web/Controllers/KeyVaultController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace KeyVaultCA.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KeyVaultController : ControllerBase
+    {
+        private readonly ILogger<KeyVaultController> _logger;
+
+        public KeyVaultController(ILogger<KeyVaultController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            // TODO
+
+            return new[] { "1", "2", "3" };
+        }
+    }
+}
